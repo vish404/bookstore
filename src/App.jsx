da70394c174cc2baa7981948d9b523f8e307af8b
Vish <vish@vbits.top>
2026-08-25 17:28:51 +0300

    Fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,25 @@
-import { useState } from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import Home from './components/Home'
+import AddBook from './components/AddBook'
 
 function App() {
-    const [todo, setTodo] = useState({ description: '', date: '' });
-    const [todos, setTodos] = useState([]);
-
-    const inputChanged = (event) => {
-        setTodo({ ...todo, [event.target.name]: event.target.value });
-    }
-
-    const addTodo = () => {
-        setTodos([...todos, todo]);
-    }
-
-    const deleteTodo = (row) => {
-        setTodos(todos.filter((todo, index) => index !== row));
-    }
-
-    return (
-        <>
-            <input
-                placeholder="Description"
-                name="description"
-                value={todo.description}
-                onChange={inputChanged} />
-            <input
-                placeholder="Date"
-                name="date"
-                value={todo.date}
-                onChange={inputChanged} />
-            <button onClick={addTodo}>Add</button>
-            <table>
-                <tbody>
-                    {
-                        todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo.description}</td>
-                                <td>{todo.date}</td>
-                                <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-                            </tr>)
-                    }
-                </tbody>
-            </table>
-        </>
-    );
+  
+  return (
+    <BrowserRouter>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">
+            Bookstore
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/add" element={<AddBook />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
